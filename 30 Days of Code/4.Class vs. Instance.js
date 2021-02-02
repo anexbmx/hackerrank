61a416a65b801732dd3d42c28d0d3cc75884cfdc
anas boumediene <anexbm@gmail.com>
2021-02-02 22:55:28 +0100

    Day 34 Class vs. Instance done! 💪

---
+++ 30 Days of Code/4.Class vs. Instance.js
@@ -0,0 +1,44 @@
+class Person {
+    constructor(initialAge) {
+        if (initialAge < 0) {
+            initialAge = 0;
+            console.log("Age is not valid, setting age to 0");
+        }
+        this.age = initialAge;
+    }
+
+    yearPasses() {
+        this.age++;
+    }
+
+    amIOld() {
+        this.age < 13
+            ? console.log("You are young..")
+            : this.age >= 13 && this.age < 18
+            ? console.log("You are a teenager..")
+            : console.log("You are old..");
+    }
+}
+
+function Person(initialAge) {
+    // Add some more code to run some checks on initialAge
+    if (initialAge < 0) {
+        initialAge = 0;
+        console.log("Age is not valid, setting age to 0.");
+    }
+    this.age = initialAge;
+
+    this.amIOld = function () {
+        // Do some computations in here and print out the correct statement to the console
+        this.age < 13
+            ? console.log("You are young.")
+            : this.age >= 13 && this.age < 18
+            ? console.log("You are a teenager.")
+            : console.log("You are old.");
+    };
+
+    this.yearPasses = function () {
+        // Increment the age of the person in here
+        this.age++;
+    };
+}
